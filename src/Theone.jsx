25a438a0e9d85b2a527fb08f496713f4c073f243
Yug Bothra <yugbothra200@outlook.com>
2025-05-13 22:53:28 +0530

    my first react project

--- src/Theone.jsx
+++ src/Theone.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Hero from './hero'
+import Hero from './Hero'
 import About from './About'
 import Projects from './Projects'
 import Experience from './Experience'
